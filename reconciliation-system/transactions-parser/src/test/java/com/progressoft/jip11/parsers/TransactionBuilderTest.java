2913c6a7643d5c324f31641b7131560ca604616a
Muhannad Alrawajfeh <muhannad.alrawajfeh@progressoft.io>
2021-04-11 18:05:41 +0300

    added parser factory and amount format

--- reconciliation-system/transactions-parser/src/test/java/com/progressoft/jip11/parsers/TransactionBuilderTest.java
+++ reconciliation-system/transactions-parser/src/test/java/com/progressoft/jip11/parsers/TransactionBuilderTest.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Currency;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TransactionBuilderTest {
 
