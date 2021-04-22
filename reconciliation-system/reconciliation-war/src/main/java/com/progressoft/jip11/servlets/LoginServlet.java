54cf1b8f2988cf37489cfd11994bfa9399829abf
Muhannad Alrawajfeh <muhannad.alrawajfeh@progressoft.io>
2021-04-22 15:02:16 +0300

    new commit

---
+++ reconciliation-system/reconciliation-war/src/main/java/com/progressoft/jip11/servlets/LoginServlet.java
@@ -0,0 +1,19 @@
+package com.progressoft.jip11.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+
+    }
+}
